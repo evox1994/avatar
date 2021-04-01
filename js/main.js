c1bd717ef00aeafefe463b571b05dc78f1feadae
evox1994 <dmitrybatukhtin@gmail.com>
2021-04-01 19:02:02 +0700

    dsvdv

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+function isHighDensity(){/*проверка HD display*/
+	return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 124dpi), only screen and (min-resolution: 1.3dppx), only screen and (min-resolution: 48.8dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (min-device-pixel-ratio: 1.3)').matches)) || (window.devicePixelRatio && window.devicePixelRatio > 1.3));
+}
+
+function isRetina(){/*проверка Retina*/
+	return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio >= 2)) && /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
+}
+
 $(document).ready(function(){
 
 	$(document).on('click','.radio-btn',function(){
@@ -510,8 +518,16 @@ $(document).ready(function(){
 			$('img[data-src]').each(function(){
 				let img_width = $(this).outerWidth();
 				$(this).attr('src',$(this).attr('data-src'));
-				$(this).css('width',img_width);
+				$(this).css({
+					'width':'100%',
+					'max-width': img_width,
+				});
 			});
+		}
+		if ( isRetina() || isHighDensity() ){
+			alert('высокое разрешение');
+		} else {
+			alert('нифига');
 		}
 	}
 	swapImage();
